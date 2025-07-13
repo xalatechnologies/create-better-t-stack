864e863656a30f46f4f93c6c7775df498de2f433
Aman Varshney <amanvarshney.work@gmail.com>
2025-07-13 13:44:09 +0530

    feat(web): migrate from pages to workers and add posthog analytics

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -5,9 +5,26 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
 	reactStrictMode: true,
-	output: "export",
+	// This is required to support PostHog trailing slash API requests
+	skipTrailingSlashRedirect: true,
 	experimental: {
 		reactCompiler: true,
+	},
+	async rewrites() {
+		return [
+			{
+				source: "/ingest/static/:path*",
+				destination: "https://us-assets.i.posthog.com/static/:path*",
+			},
+			{
+				source: "/ingest/:path*",
+				destination: "https://us.i.posthog.com/:path*",
+			},
+			{
+				source: "/ingest/decide",
+				destination: "https://us.i.posthog.com/decide",
+			},
+		];
 	},
 };
 
